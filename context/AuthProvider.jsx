b7e5ee0e8cc4c9d194e6a9669a34209115c2a668
Andres Reyes <andresreyes@MacBook-Pro-de-Andres.local>
2024-08-18 11:07:22 -0400

    prueba de envío de formulario

--- context/AuthProvider.jsx
+++ context/AuthProvider.jsx
@@ -9,67 +9,67 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
 
     const [auth, setAuth] = useState({});
-    const [cargando, setCargando] = useState(true);
+    const [cargando, setCargando] = useState(false);
     const navigate = useNavigate();
     const [darkTheme, setDarkTheme] = useState()
 
     // Utilizamos el useEffect para autorizar a los usuarios con el Token
     //Para eso se consulta la API con el token y se guarda los datos de Auth en el state
-    useEffect(() => {
+    // useEffect(() => {
 
-        // Verificamos el theme
-        const theme = localStorage.getItem('theme')
-        if (theme === 'true') {
-            setDarkTheme(true)
-            document.querySelector('html').classList.add('dark')
-        } else {
-            setDarkTheme(false)
-            document.querySelector('html').classList.remove('dark')
-        }
-
-
-        const autenticarUsuario = async () => {
-
-            const tokenAPI = await getAuthToken(); // Obtén el token usando la función
-            const token = await localStorage.getItem('tokenUser');
-
-            if (!token) {
-                setCargando(false);
-                return;
-            }
-
-            const configWithTokenAPI = {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: tokenAPI
-                }
-            };
+    //     // Verificamos el theme
+    //     const theme = localStorage.getItem('theme')
+    //     if (theme === 'true') {
+    //         setDarkTheme(true)
+    //         document.querySelector('html').classList.add('dark')
+    //     } else {
+    //         setDarkTheme(false)
+    //         document.querySelector('html').classList.remove('dark')
+    //     }
 
 
-            // Realizamos una consulta a la API
-            try {
-                const { data } = await axios.post('http://172.206.55.212:4001/api/users/verify-token', { token }, configWithTokenAPI);
-                const user = data.user
+    //     const autenticarUsuario = async () => {
 
-                // Guardamos los datos del usuario en el contexto
-                setAuth(user)
+    //         const tokenAPI = await getAuthToken(); // Obtén el token usando la función
+    //         const token = await localStorage.getItem('tokenUser');
 
-                // Reenviamos al usuario si no existe el contexto
-                if (!user.id) {
-                    navigate('/app');
-                }
+    //         if (!token) {
+    //             setCargando(false);
+    //             return;
+    //         }
 
-                setCargando(false);
+    //         const configWithTokenAPI = {
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 Authorization: tokenAPI
+    //             }
+    //         };
 
-            } catch (error) {
-                setAuth({});
-                setCargando(false);
-            }
-        }
 
-        autenticarUsuario(); // Llamamos la función
+    //         // Realizamos una consulta a la API
+    //         try {
+    //             const { data } = await axios.post('http://172.206.55.212:4001/api/users/verify-token', { token }, configWithTokenAPI);
+    //             const user = data.user
 
-    }, []);
+    //             // Guardamos los datos del usuario en el contexto
+    //             setAuth(user)
+
+    //             // Reenviamos al usuario si no existe el contexto
+    //             if (!user.id) {
+    //                 navigate('/app');
+    //             }
+
+    //             setCargando(false);
+
+    //         } catch (error) {
+    //             setAuth({});
+    //             setCargando(false);
+    //         }
+    //     }
+
+    //     autenticarUsuario(); // Llamamos la función
+
+    // }, []);
 
 
 
